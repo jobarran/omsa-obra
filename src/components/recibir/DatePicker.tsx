3542da9609645d0fbaf6cba2818009e5c5742f55
Joaquin Barrandeguy <joaquinbarrandeguy@gmail.com>
2024-04-22 12:31:47 -0300

    UI recibir table 1.01

--- src/components/recibir/DatePicker.tsx
+++ src/components/recibir/DatePicker.tsx
@@ -24,7 +24,7 @@ export const DatePicker = ({value, setValue}: Props) => {
       <Datepicker
         asSingle={true}
         useRange={false} // Indicates whether to use a range of dates or single date selection
-        inputClassName="bg-white border border-gray-200 text-gray-900 sm:text-sm rounded-lg  w-full h-10 p-2.5"
+        inputClassName="bg-white border border-gray-200 text-gray-900 sm:text-sm rounded-lg w-full h-10 p-2.5"
         placeholder={"Elegir Fecha"} // Placeholder text for the input field
         value={value} // Selected date(s) value
         onChange={handleValueChange} // Event handler for date selection changes
