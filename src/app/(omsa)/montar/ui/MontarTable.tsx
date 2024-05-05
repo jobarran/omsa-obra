24b5e759d5d67d42787df55ef8c257ba91379a73
Joaquin Barrandeguy <joaquinbarrandeguy@gmail.com>
2024-05-05 13:33:07 -0300

    Montar 1.1

--- src/app/(omsa)/montar/ui/MontarTable.tsx
+++ src/app/(omsa)/montar/ui/MontarTable.tsx
@@ -2,8 +2,8 @@
 
 import { useMaterialStore, useUiStore } from "@/store"
 import { useEffect, useState } from 'react';
-import { ButtonCard, ButtonCardUploadRemito, CodeRepeatedError, DatePicker, EmptyTableButton, ManualAddMaterial, MontarAddButton, MontarDatePicker, MontarFloorAndPossition, MontarMatPrev, MontarObservations, RecibirTableBody, ReturnMaterialModal, SaveButton } from "@/components";
-import { checkDuplicates, checkObra, getTodayDate, qrToRecibir } from "@/utils";
+import { ButtonCard, ButtonCardUploadRemito, CodeRepeatedError, EmptyTableButton, ManualAddMaterial, MontarAddButton, MontarDatePicker, MontarFloorAndPossition, MontarMatPrev, MontarObservations, RecibirTableBody, ReturnMaterialModal, SaveButton } from "@/components";
+import { checkDuplicates, checkObra, getTodayDate } from "@/utils";
 import { createMaterials, getMaterialsByProject } from "@/actions";
 import { SavedSuccessMessage } from '../../../../components/recibir/SavedSuccessMessage';
 import MontarQrReader from "@/components/qr/MontarQrReader";
@@ -44,28 +44,7 @@ export const MontarTable = () => {
   });
 
   const handleSaveMaterials = async () => {
-    if (storeMaterial) {
-      const { materials } = await getMaterialsByProject(activeProject?.code);
-
-      const duplicates = checkDuplicates(materials, storeMaterial);
-      const difObra = checkObra(activeProject?.code, storeMaterial);
-
-      if (duplicates.length > 0) {
-        setIsMaterialError(`Alguno de los materiales que cargaste ya figuran como recibidos`);
-        const duplicatedCodesArray = duplicates.map(material => material.code);
-        setErrorCodes(duplicatedCodesArray);
-      } else if (difObra.length > 0) {
-        setIsMaterialError('El material que esta intentando cargar corresponde a otra obra');
-        const difObraCodesArray = difObra.map(material => material.code);
-        setErrorCodes(difObraCodesArray);
-
-      } else {
-        // If no duplicates or materials from different obra are found, proceed with saving the materials
-        setIsMaterialSavedSuccess();
-        await createMaterials(storeMaterial, value.startDate);
-        emptyStoreMaterial();
-      }
-    }
+    
   };
 
   const handleMontarMaterialChange = (data: any) => {
